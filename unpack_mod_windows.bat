9437d46655a084d0b6d7fa342b683d5c5d6b33a0
Caelan L. Talmadge <34071000+cltalmadge@users.noreply.github.com>
2022-02-27 23:30:46 -0500

    Fixed my retardation.
    Zoltan sucks

--- unpack_mod_windows.bat
+++ unpack_mod_windows.bat
@@ -1,2 +1,2 @@
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 unpack --file Amia.mod --yes"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 unpack --file Amia.mod --default"
 echo Finished.

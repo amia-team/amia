42328074d580ed44eed0f3fef1caa47ec9717e93
Caelan L. Talmadge <34071000+cltalmadge@users.noreply.github.com>
2022-03-30 04:32:50 -0400

    Delete build_linux.sh

--- build_linux.sh
+++
@@ -1,4 +0,0 @@
-git pull origin master;
-docker run --rm -it -v $(pwd):/nasher urothis/nwnee-community-images:nasher-8193.34 pack --clean --verbose;
-mv ../amia_server/test_server/modules/Amia.mod ../amia_server/test_server/modules/Amia.mod.bak;
-cp Amia.mod ../amia_server/test_server/modules;

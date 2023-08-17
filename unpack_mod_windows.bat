c9a91836bba27d9e53d0ce730ad4dba33a16bea4
Caelan L. Talmadge <34071000+cltalmadge@users.noreply.github.com>
2023-08-17 12:16:39 -0500

    Update unpack_mod_windows.bat

--- unpack_mod_windows.bat
+++ unpack_mod_windows.bat
@@ -1,2 +1,2 @@
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 unpack --file Amia.mod --default"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -t -u ${id -u}:${id -g} -v ${pwd}:/nasher cltalmadge/nasher:0.20.2 unpack --file Amia.mod --default"
 echo Finished.

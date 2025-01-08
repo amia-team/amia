3999d1817a4218b9d809a6da98d4768b52b00628
Caelan L. Talmadge <talmadge12@gmail.com>
2025-01-08 11:30:54 -0600

    Fix windows mod packing container version

--- pack_mod_windows.bat
+++ pack_mod_windows.bat
@@ -1,2 +1,2 @@
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -t -u ${id -u}:${id -g} -v ${pwd}:/nasher cltalmadge/nasher:0.20.2 pack --clean --verbose --yes"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -t -u ${id -u}:${id -g} -v ${pwd}:/nasher cltalmadge/nasher:1.1.1 pack --clean --verbose --yes"
 echo Finished.

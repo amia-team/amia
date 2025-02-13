f516dd9f12f0293a5de5c4efc703b492799878a7
Caelan L. Talmadge <talmadge12@gmail.com>
2025-02-13 12:56:34 -0600

    Add linuxed base generic unpacker

---
+++ unpack.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for file in "$@"; do
+    filename=$(basename "$file")
+    docker run --rm -t -u $(id -u):$(id -g) -v "$(pwd)":/nasher cltalmadge/nasher:1.1.1 unpack --file "$filename" --removeDeleted:false --yes
+done
+
+echo "Finished."
